4a39ee8f62d365de9d7ece06451ca4c657f2149a
Jessica Naiara <jessicanaiarag@gmail.com>
2021-05-21 18:29:44 -0300

    exercicio pratico 3

--- bloco_4/dia_3/exerciciopratico3.js
+++ bloco_4/dia_3/exerciciopratico3.js
@@ -1,9 +1,16 @@
-let tamanho = 5;  
-let linha = '';
+let linhas = 5;  
+let asteriscos = '';
+let posicao = linhas;
 
-for (let index1 = tamanho; index1 > 1; index1 -=1) {
-    linha = linha + '-';
+for (let index1 = 0; index1 <= linhas; index1 +=1) {
+    for (let index2 = 0; index2 <= linhas; index2 += 1) {
+        if (index2 < posicao) {
+            asteriscos = asteriscos + ' ';
+    } else {
+        asteriscos = asteriscos + '*';
+    }
+    }
+    console.log(asteriscos);
+    asteriscos = '';
+    posicao -= 1; 
 }
-
-console.log(linha+'*');
-
