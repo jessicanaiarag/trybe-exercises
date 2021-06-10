40a8dd744259fda3c565c4e5c3498e1bcc46b263
Jessica Naiara <jessicanaiarag@gmail.com>
2021-06-10 14:43:06 -0300

    exercicio 1

---
+++ bloco_6/dia_2/exercicio-1/library.js
@@ -0,0 +1,7 @@
+// let moment = require('moment');
+// let brasilTetra = moment('17071994', 'DDMMYYYY');
+// let fromnow = brasilTetra.fromNow();
+// console.log('Ouvimos o É TETRAAA!!! HA: ' + fromnow); 
+cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+    alert('WOW QUE MARAVILHA!');
+});
