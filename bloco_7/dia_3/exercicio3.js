a7dcf9f49f8cc5270f7c4a0fd1233d11933fb1ef
Jessica Naiara <jessicanaiarag@gmail.com>
2021-06-21 20:07:32 -0300

    exercicio 3

---
+++ bloco_7/dia_3/exercicio3.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+// implemente seus testes aqui
+assert.deepStrictEqual(myRemoveWithoutCopy([1,2,3,4],3), [1,2,4]);
+
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1,2,3,4],3), [1,2,3,4]);
+
+const a = [1,2,3,4];
+
+myRemoveWithoutCopy(a, 2);
+
+assert.notDeepStrictEqual(a, [1,2,3,4]);
+
+assert.deepStrictEqual(myRemoveWithoutCopy([1,2,3,4],5), [1,2,3,4]);
