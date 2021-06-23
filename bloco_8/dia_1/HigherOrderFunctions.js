e66a7c621b9fd7a3a6a57e2d7432c8f354c425c1
Jessica Naiara <jessicanaiarag@gmail.com>
2021-06-23 15:18:39 -0300

    exemplo de HOF

--- bloco_8/dia_1/HigherOrderFunctions.js
+++ bloco_8/dia_1/HigherOrderFunctions.js
@@ -2,12 +2,19 @@ const repeat = (number, action) => {
     for (let count = 0; count <= number; count += 1) {
       action(count);
     }
- };
+  };
   
-  //repeat(7, console.group);
-
-  repeat(7, (number) => {
+  const isEven = (number) => {
     if (number % 2 === 0) {
       console.log(number, 'is even');
     }
-  });
+  };
+  
+  const isOdd = (number) => {
+    if ((number % 2) > 0) {
+      console.log(number, 'is odd');
+    }
+  };
+  
+  repeat(3, isEven); // Testa quais números serão pares;
+  repeat(3, isOdd); // Testa quais números serão ímpares;
