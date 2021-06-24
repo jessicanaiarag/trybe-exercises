a2db087bb37c88bece94370dd335cf0a616240fa
Jessica Naiara <jessicanaiarag@gmail.com>
2021-06-24 18:09:34 -0300

    aula ao vivo

--- bloco_8/dia_2/aula.js
+++ bloco_8/dia_2/aula.js
@@ -40,3 +40,46 @@ const notDrive = users.find((user) => {
 console.log(notDrive)
 
 console.log(`O primeiro usuário que não sabe dirigir é ${firstNotDrive.firstName}`);
+
+const alguemQNDrive = users.some((user) => {
+
+    return user.isDriver === false;
+})
+console.log(`existe alguem que não dirige? ${alguemQNDrive}`);
+
+/*/let everyOneDrive = true;
+for (user of users) {
+   if(user.isDriver === false) {
+    everyOneDrive === false
+    break;
+   }   
+}*/
+
+
+const everyOneDrive = users.every((value) => {
+    return user.isDriver === true;
+})
+
+console.log(`Todos sabem dirigir? ${everyOneDrive}`);
+
+const names = ['wallisson', 'Amanda', 'Tiago', 'Leticia'];
+console.log(`antes: ${names}`);
+names.sort();
+console.log(`depois: ${names}`);
+
+const number = [1 , 4, 8, 9, 3, 4];
+console.log(`antes: ${number}`);
+number.sort((value1, value2) => {
+    return value1 - value2;
+});
+console.log(`depois enxuta: ${number}`);
+
+const users1 = [
+    {firstName: 'Homer', lastName: 'Simpson', isDriver: true, age: 40},
+    {firstName: 'Bart', lastName: 'Simpson', isDriver: false, age: 15},
+    {firstName: 'Marge', lastName: 'Simpson', isDriver: true, age:41},
+];
+
+users1.sort((a,b) => a - b);
+
+console.log(users1.sort(users1.age));
