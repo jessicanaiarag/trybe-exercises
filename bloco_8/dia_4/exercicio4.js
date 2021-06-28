6594827f5320a815bb4d7349de8c64edc4f0b10b
Jessica Naiara <jessicanaiarag@gmail.com>
2021-06-28 20:29:02 -0300

    exercicio 4

--- bloco_8/dia_4/exercicio4.js
+++ bloco_8/dia_4/exercicio4.js
@@ -63,9 +63,25 @@ const books = [
   },
 ];
 
+
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
 // Adicione o código do exercício aqui:
 function longestNamedBook() {
     // escreva seu código aqui
+    const maiorNameBook = books.reduce((acc, curr) =>{
+      return acc.name.length > curr.name.length ? acc : curr;
+    })
+    return maiorNameBook;
   }
   
   assert.deepStrictEqual(longestNamedBook(), expectedResult);
