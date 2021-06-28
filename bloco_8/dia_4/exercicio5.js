e92a9a536d163900e3be67b9903e4ea4f42466a3
Jessica Naiara <jessicanaiarag@gmail.com>
2021-06-28 20:43:30 -0300

    exercicio 5

--- bloco_8/dia_4/exercicio5.js
+++ bloco_8/dia_4/exercicio5.js
@@ -8,6 +8,12 @@ const names = [
 
 function containsA() {
   // escreva seu código aqui
+  const letrasM = names.reduce((acc, curr) => {
+    const qtdA = curr.split('').filter((letra) => letra.toUpperCase() === 'A').length;
+    acc += qtdA;
+    return acc;
+  }, 0)
+  return letrasM;
 }
 
 assert.deepStrictEqual(containsA(), 20);
