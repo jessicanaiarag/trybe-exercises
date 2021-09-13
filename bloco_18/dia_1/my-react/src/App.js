bab339716dba1d15f7f7d90d6d23f488110ec286
Jessica Naiara <jessicanaiarag@gmail.com>
2021-09-13 20:54:36 -0300

    exercicio rocketseat

---
+++ bloco_18/dia_1/my-react/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Login from "./components/Login";
+import Profile from "./components/profile";
+import { useAuth } from "./providers/auth";
+
+function App() {
+  const { user } = useAuth();
+
+  return (
+    <div className="App">
+      <h1>Ola mundo</h1>
+      <Profile />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ bloco_18/dia_1/my-react/src/components/Login.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { useAuth } from '../providers/auth';
+
+
+function Login() {
+  const [ input, setInput ] = useState({
+      name:"",
+  })  
+  const { setUser } = useAuth(); 
+
+  const handleLogin = () => {
+      localStorage.setItem('user', JSON.stringify('user'));
+      setUser(input);
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    setUser({
+        name: '',
+    });
+}
+
+
+
+  return (
+  <div>
+       <input type="text" onChange={(event) => setInput ({name: event.target.value})} />
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogout}>Logout</button>
+  </div>
+  )
+};
+
+export default Login;                 
